aabe0cee40563be2544bdf6ae16310ced9712393
OTPStrafe <otpstrafe@gmail.com>
2020-09-14 01:22:27 -0300

    hotfix

--- src/bot/commands/info.js
+++ src/bot/commands/info.js
@@ -4,14 +4,15 @@ data = {
     description: `Cualquier duda sobre los comandos puedes escribirle a Rich#3505
 
                   Cualquier bug o problema tambien puedes escribirle a Rich#3505
-                  
+
                   Si quieres invitar al bot a tu servidor puedes usar el siguiente [Enlace](https://robocatjs-discord.herokuapp.com/invite)`,
     fieldName1: 'Comandos Generales',
     fieldValue1: '-info  -8ball  -meme  -play  -roll  -dolar' ,
     fieldName2: 'Comandos NSFW' ,
     fieldValue2: '-poto  -hentai  -tetica' ,
     fieldName3: 'Comandos Anime' ,
-    fieldValue3: '-buscaranime  -buscarmanga'
+    fieldValue3: '-buscaranime  -buscarmanga',
+    test: 'test'
 } 
 
 module.exports = {
